6fe6b1d52452d2e680a1327c2f10f0496144c4ea
Pengwei Chen <Pengwei.19@intl.zju.edu.cn>
2020-03-18 11:35:12 +0800

    Revise

--- Practical6/Counting DNA nucleotides.py
+++ Practical6/Counting DNA nucleotides.py
@@ -1,12 +1,21 @@
-import matplotlib.pyplot as plt
+#initialize
+import matplotlib.pyplot as plt 
+
+#get input
 A=input("A=")
 C=input("C=")
 G=input("G=")
 T=input("T=")
+
+#create dictionary
 dictionary={"A":A,"C":C,"G":G,"T":T}
+
+#properties of the pie chart
 labels="A","C","G","T"
 sizes=A,C,G,T
 explode=(0,0,0,0)
+
+#output
 print(dictionary)
 plt.pie(sizes,explode=explode,labels=labels,autopct="%1.1f%%",
         shadow=True,startangle=50)
--- Practical6/List sorting and filtering.py
+++ Practical6/List sorting and filtering.py
@@ -1,12 +1,28 @@
+#initialize
 import matplotlib.pyplot as plt
+
+#input
 str=input("gene_lengths=")
+
+#simple processing of input
 str=str.strip("[]")
+
+#divide and transfer into "Length"
 Length=str.split(",")
+
+#digitalize
 for i in range(0,len(Length)):
     Length[i]=int(Length[i])
+
+#sort
 Length.sort()
+Length.reverse()
+
+#delete the longest	and	shortest
 del(Length[0])
 del(Length[len(Length)-1])
+
+#output
 print(Length)
 plt.boxplot(Length,
             vert=True,
